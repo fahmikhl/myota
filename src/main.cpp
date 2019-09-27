b3bb9865a46047ec2bb43a2336bfee5980d27719
fahmikhl <fahmikoerullatif@gmail.com>
2019-09-27 19:18:11 +0700

    update travis and pio setting

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ void loop() {
   digitalWrite(LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
   // but actually the LED is on; this is because
   // it is active low on the ESP-01)
-  delay(1000);                      // Wait for a second
+  delay(100);                      // Wait for a second
   digitalWrite(LED, HIGH);  // Turn the LED off by making the voltage HIGH
-  delay(1000);                      // Wait for two seconds (to demonstrate the active low LED)
+  delay(500);                      // Wait for two seconds (to demonstrate the active low LED)
 }
