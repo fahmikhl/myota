703b0287598188e0ec8a74735394d8effa8cda4c
fahmikhl <fahmikoerullatif@gmail.com>
2019-10-20 18:33:46 +0700

    Road to Success

--- src/main.cpp
+++ src/main.cpp
@@ -51,40 +51,53 @@ void getVersion(){
       terminal.clear();
       HTTPClient http;  //Object of class HTTPClient
       http.begin("https://api.github.com/repos/fahmikhl/myota/releases/latest","59 74 61 88 13 CA 12 34 15 4D 11 0A C1 7F E6 67 07 69 42 F5");
-      
+      delay(500);
       int httpCode = http.GET();                   
       if (httpCode > 0) {
         Serial.println(httpCode);
-        String payload = http.getString();   //Get the request response payload
-      //  Serial.println(payload);  
-        // Parsing
+        String payload = http.getString(); 
+        delay(1000);
+        //  Serial.println(payload);
+
+        //===================== Parsing=======================
         const size_t capacity = JSON_ARRAY_SIZE(1) + JSON_OBJECT_SIZE(13) + 3*JSON_OBJECT_SIZE(18) + 3120;
         DynamicJsonBuffer jsonBuffer(capacity);
         JsonObject& root = jsonBuffer.parseObject(payload);
         const char* tag_name = root["tag_name"]; 
       // const char* name = root["name"]; 
-        Serial.println("Lastest Version : ");
+        Serial.print("Lastest Version : ");
         Serial.println(tag_name);
         terminal.println("\nLastest Version :\n ");
         terminal.print(tag_name);
         if (buildTag != tag_name){
           download = false;
-          Serial.println("Update Required !!");
+          Serial.print("Update Required !!");
           terminal.println("\nUpdate Required !!\n ");
           terminal.flush();
           } 
         else {
           Serial.print("No Update needed !");
         }
-      http.end();   //Close connection
+        http.end();   //Close connection
+       }
+      else
+      {
+        Serial.println("URL Connection Failed !!");
       }
     } 
     doUpdateCheck = false;
   }
+  else
+  {
+     Serial.println("Waiting...");
+     delay(5000);
+  }
+  
 }
 
 void DownloadBin(){
- if (download == false){
+ //if (download == false){
+if (doUpdateCheck == true) { 
   if (WiFi.status() == WL_CONNECTED) {
     Serial.println("Downloading Firmware...");
     terminal.println("\nDownloading Firmware...\n");
@@ -112,8 +125,10 @@ void DownloadBin(){
           terminal.flush();
       }
     }
+  doUpdateCheck = false;
  }
 }
+
 
 BLYNK_WRITE(V25){
     if (param.asInt()) {  
@@ -134,13 +149,16 @@ void setup(){
   setWifi();
   pinMode(ESP_LED, OUTPUT);
   Blynk.begin(auth, ssid, pass);
-  Serial.println("BUILD_TAG: "+ buildTag);
+  Serial.println("Current Version: "+ buildTag);
+  terminal.println("\nCurrent Version: ");
+  terminal.println(buildTag);
+  terminal.flush();
   updateCheck.start();
 }
 
 void loop(){
   updateCheck.update();  
   Blynk.run();
-  getVersion();
+ // getVersion();
   DownloadBin();
 }
