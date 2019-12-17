6b2010726404a2fd66c3041d082408a4b7c17fc7
fahmikhl <fahmikhoerullatif@gmail.com>
2019-12-17 16:15:49 +0700

    test nama 2

--- src/main.cpp
+++ src/main.cpp
@@ -52,56 +52,7 @@ void setWifi(){
      // digitalWrite(ESP_LED, LOW);
   }
 }
-//====================================Get-Version============================
-void getVersion(){
-  if (doUpdateCheck == true) {  
-    if (WiFi.status() == WL_CONNECTED) {
-      terminal.clear();
-      HTTPClient http; 
-      http.begin("https://api.github.com/repos/fahmikhl/myota/releases/latest","59 74 61 88 13 CA 12 34 15 4D 11 0A C1 7F E6 67 07 69 42 F5");
-      delay(500);
-      int httpCode = http.GET();                   
-      if (httpCode > 0) {
-        Serial.println(httpCode);
-        String payload = http.getString(); 
-        delay(1000);
-        //  Serial.println(payload);
 
-        //===================== Parsing=======================
-        const size_t capacity = JSON_ARRAY_SIZE(1) + JSON_OBJECT_SIZE(13) + 3*JSON_OBJECT_SIZE(18) + 3120;
-        DynamicJsonBuffer jsonBuffer(capacity);
-        JsonObject& root = jsonBuffer.parseObject(payload);
-        const char* tag_name = root["tag_name"]; 
-      // const char* name = root["name"]; 
-        Serial.print("Lastest Version : ");
-        Serial.println(tag_name);
-        terminal.println("\nLastest Version :\n ");
-        terminal.print(tag_name);
-        if (buildTag != tag_name){
-          download = false;
-          Serial.print("Update Required !!");
-          terminal.println("\nUpdate Required !!\n ");
-          terminal.flush();
-          } 
-        else {
-          Serial.print("No Update needed !");
-        }
-        http.end();   //Close connection
-       }
-      else
-      {
-        Serial.println("URL Connection Failed !!");
-      }
-    } 
-    doUpdateCheck = false;
-  }
-  else
-  {
-     Serial.println("Waiting...");
-     delay(5000);
-  }
-  
-}
 //=====================================Download Firmware=====================================
 void DownloadBin(){
 
@@ -153,7 +104,6 @@ void setup(){
   terminal.flush();
   updateCheck.start();
 }
-
 
 void loop(){
   updateCheck.update();  
